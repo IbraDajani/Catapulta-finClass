1dc6aaa4788cc5041f1a97e5ea12027a613545a0
Ibrahim Dajani <ibrahimdajani121@gmail.com>
2022-04-03 02:39:16 -0300

    Create button and separator components

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { useMemo } from "react";
+import { useTheme } from "styled-components";
+
+import { Container, SpacingIcon, Title } from "./styles";
+import { Props } from "./types";
+
+const Button = ({
+  disabled,
+  title,
+  color,
+  icon,
+  onPress,
+  outlined,
+  textColor,
+  textType = "regular",
+}: Props) => {
+  const { colors } = useTheme();
+
+  const titleColor = useMemo(() => {
+    if (textColor) return textColor;
+    if (disabled) return colors.backdrop.main;
+    return outlined ? colors.backdrop.main : colors.background.main;
+  }, [textColor, disabled, outlined, colors]);
+
+  return (
+    <Container
+      onPress={onPress}
+      disabled={disabled}
+      outlined={outlined || disabled}
+      color={color}
+    >
+      {!!icon && (
+        <>
+          {icon}
+          <SpacingIcon />
+        </>
+      )}
+      <Title color={titleColor} type={textType}>
+        {title}
+      </Title>
+    </Container>
+  );
+};
+
+export default Button;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,21 @@
 import { StatusBar } from "expo-status-bar";
+import React from "react";
 import { View } from "react-native";
 import { useTheme } from "styled-components";
 import Text from "./components/Text";
 import { Container } from "./styles";
+import Button from "./components/Button";
+import Separator from "./components/Separator";
 
 const App = () => {
-  const { typography } = useTheme();
+  const {} = useTheme();
   return (
     <>
       <StatusBar style="auto" />
       <Container>
         <Text>Hello</Text>
+        <Separator height={20} />
+        <Button textType="bold" title="Assine agora"></Button>
       </Container>
     </>
   );
